36b9d0c3dcdca16cbb1e786be2d4bc4f92393524
Ted LAYHER <phred@Bimetallic.lan>
2021-12-06 23:43:51 -0500

    Day 6 part 1

---
+++ day6/day6.py
@@ -0,0 +1,32 @@
+
+
+def part1():
+    lantern_fish = []
+
+    with open("./short.txt", "r") as f:
+        for line in f:
+            lantern_fish = list(map(int, line.split(',')))
+    print(lantern_fish)
+    for day in range(1,257):
+        f =[]
+        new_fish = []
+        print(F'Day {day}')
+        for l in lantern_fish:
+            if(l == 0):
+                f.append(6)
+                new_fish.append(8)
+            else:
+                f.append(l -1)
+        lantern_fish = f.extend( new_fish)
+        print(len(lantern_fish))
+
+        
+            
+
+
+
+
+
+
+
+part1()
