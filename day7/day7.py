d9b2ae5f75a9ef8e4e75722b2eb2298f5cce7f63
Ted LAYHER <phred@Bimetallic.lan>
2021-12-07 08:38:31 -0500

    Update day 7 to actually use median

--- day7/day7.py
+++ day7/day7.py
@@ -6,18 +6,21 @@ def part1():
     with open("./long.txt", "r") as f:
         for line in f:
             crab_submarines = list(map(int, line.split(',')))
-    most_common_position = Counter(crab_submarines).most_common(1)[0]
-    print(Counter(crab_submarines).most_common(5))
-    print(F'Median: {statistics.median(crab_submarines)}')
-    print(F'Most common postion: {most_common_position}')
-    most_common_position = 330
+    median_position = int(statistics.median(crab_submarines))
+    print(F'Median position: {median_position}')
     fuel = 0
     for c in crab_submarines:
-        fuel += abs(c - most_common_position)
+        fuel += abs(c - median_position)
             
     print(F'Fuel used: {fuel}')
     # 459589 is too high
     # 430623 is still too high
+
+
+def part2():
+    with open("./long.txt", "r") as f:
+        for line in f:
+            crab_submarines = list(map(int, line.split(',')))
 
 if __name__ == '__main__':
     part1()
